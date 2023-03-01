8bbdf0888f0cb7cb624466d9ec547cabff2fde74
IvanShastakouSAP <ivan.shastakou@sap.com>
2023-03-01 11:36:21 +0300

    Javadoc refactored

--- cloudfoundry-client/src/main/java/org/cloudfoundry/client/v3/processes/Processes.java
+++ cloudfoundry-client/src/main/java/org/cloudfoundry/client/v3/processes/Processes.java
@@ -41,10 +41,10 @@ public interface Processes {
 
 
     /**
-     * Makes the <a href="https://v3-apidocs.cloudfoundry.org/version/3.131.0/index.html#list-sidecars-for-process">Get stats for a process</a> request
+     * Makes the <a href="https://v3-apidocs.cloudfoundry.org/version/3.131.0/index.html#list-sidecars-for-process">List sidecars for a process</a> request
      *
-     * @param request the Get Statistics for a Process request
-     * @return the response from the Get Statistics for a Process request
+     * @param request the List Sidecars for a Process request
+     * @return the response from the List Sidecars for a Process request
      */
     Mono<ListProcessSidecarsResponse> listSidecars(ListProcessSidecarsRequest request);
 
